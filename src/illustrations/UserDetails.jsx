f210174890611825ffe35c74868a93eda9cf6c14
moria <moria.am@gmail.com>
2016-05-10 13:59:32 +0300

    Add icons

---
+++ src/illustrations/UserDetails.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default () => (
+  <svg width="100px" height="100px" viewBox="0 0 100 100" version="1.1">
+      <title>User details</title>
+      <g stroke="none" stroke-width="1" fill="#EFF4F9">
+        <path d="M100,50 C100,77.614 77.614,100 50,100 C22.386,100 0,77.614 0,50 C0,22.386 22.386,0 50,0 C77.614,0 100,22.386 100,50" id="Fill-1"></path>
+      </g>
+  </svg>
+)
